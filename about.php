e20d823e24c1da8772a79d7103906f025a59a46d
kkris26 <krisnuartha09@gmail.com>
2023-12-02 15:34:22 +0800

    update about us tambah footer

--- about.php
+++ about.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <title>Deus | About</title>
 
@@ -38,18 +39,18 @@
       <div></div>
     </div>
   </div>
-  
+
   <!-- Bg Overlay -->
   <div class="content-overlay"></div>
 
 
   <main class="main oh" id="main">
-        
- <!-- Navigation -->
- <?php include "navbar.php" ?>
+
+    <!-- Navigation -->
+    <?php include "navbar.php" ?>
     <!-- end navigation -->
 
-      
+
 
     <!-- Breadcrumbs -->
     <div class="container">
@@ -60,172 +61,175 @@
         <li class="breadcrumbs__item">
           <a href="about.php" class="breadcrumbs__url">About us</a>
         </li>
-        </ul>
+      </ul>
     </div>
 
-    <div class="main-container container" id="main-container">            
-    <section class="section section-bg" id="call-to-action" style="background-image: url(assets/images/banner.jpg)">
+    <div class="main-container container" id="main-container">
+      <section class="section section-bg" id="call-to-action" style="background-image: url(assets/images/banner.jpg)">
         <div class="container">
-            <div class="row">
-                <div class="col-lg-10 offset-lg-1">
-                    <div class="cta-content">
-                        <br>
-                        <br>
-                        <h2><em>About Us</em></h2>
-                        <p>Selamat datang di Berita Online</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <!-- ***** Our Classes Start ***** -->
-    <section class="section" id="our-classes">
-        <div class="container">
-            <br>
-            <br>
-            <br>
-            <div class="row" id="tabs">
-              <div class="col-lg-4">
-                <ul>
-                  <li><a href='#tabs-1'><i class="fa fa-globe"></i> Pengertian</a></li>
-                  <li><a href='#tabs-2'><i class="fa fa-info-circle"></i> Tujuan</a></a></li>
-                  <li><a href='#tabs-3'><i class="fa fa-briefcase"></i> Berita Online </a></a></li>
-                </ul>
-              </div>
-              <div class="col-lg-8">
-                <section class='tabs-content'>
-                  <article id='tabs-1'>
-                    <img src="assets/images/about1.jpg" alt="">
-                    <h4>Pengertian</h4>
-
-                    <p>Laporan mengenai suatu peristiwa yang dikemas oleh media sebagi sarananya yang berisi mengenai sebuah peristiwa atau kejadian yang akan dipublikasikan berupa berita ( news )</p>
-                    <p>tahap penting bagi konvergensi media (media convergence). Berita online memiliki potensi untuk memanfaatkan berbagai fitur baru dari dunia pencampuran komunikasi media digital, di antaranya audio, video, animasi, dan peningkatan kontrol pengguna (user control). </p>
-                   
-                    <p>jenis berita yang disebarkkan secara online dalam bentuk teks, audio, video, dan live streaming.beritanya yang selalu diperbaharui secara berkelanjutan (continuous updates), memiliki interaktivitas, hypertext, dan multimedia.</p>
-                  </article>
-                  <article id='tabs-2'>
-                    <img src="assets/images/about2.jpg" alt="">
-                    <h4>Tujuan</h4>
-                    <p>digunakan sebagai sarana penyampaian informasi yang cepat, dan tidak terbatas ruang serta waktu. Misalnya masyarakat Indonesia bisa mengetahui peristiwa terkini yang sedang terjadi di Palestina. Begitu pula dengan warga negara lainnya yang bisa mengetahui informasi terkini tentang Indonesia.</p>
-                    <p>dengan berbagai fitur baru dari dunia pencampuran komunikasi media digital, di antaranya audio, video, animasi, dan peningkatan kontrol pengguna (user control). </p>
-                  </article>
-                  <article id='tabs-3'>
-                    <img src="assets/images/about3.jpg" alt="">
-                    <h4>Berita Online</h4>
-                    <p>Fusce laoreet malesuada rhoncus. Donec ultricies diam tortor, id auctor neque posuere sit amet. Aliquam pharetra, augue vel cursus porta, nisi tortor vulputate sapien, id scelerisque felis magna id felis. Proin neque metus, pellentesque pharetra semper vel, accumsan a neque.</p>
-
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro aut beatae commodi repudiandae distinctio, magnam blanditiis reiciendis vitae velit voluptatum natus, fugit quis eos dolores!</p>
-
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic adipisci reiciendis quaerat qui earum aut, atque esse quisquam quis exercitationem sapiente, dolorum consequatur consequuntur voluptatibus ipsam, fuga magnam beatae optio nam. Recusandae ut aliquid, eligendi.</p>
-                  </article>
-                </section>
+          <div class="row">
+            <div class="col-lg-10 offset-lg-1">
+              <div class="cta-content">
+                <br>
+                <br>
+                <h2><em>About Us</em></h2>
+                <p>Selamat datang di Berita Online</p>
               </div>
             </div>
+          </div>
         </div>
-    </section>
-    <!-- ***** Our Classes End ***** -->
-    
-  <section class="section" id="trainers">
-      <div class="container">
-        <h1><i class="fa fa-group" style="font-size:48px;color:black"></i>Team</i></h1>
+      </section>
+
+      <!-- ***** Our Classes Start ***** -->
+      <section class="section" id="our-classes">
+        <div class="container">
+          <br>
+          <br>
+          <br>
+          <div class="row" id="tabs">
+            <div class="col-lg-4">
+              <ul>
+                <li><a href='#tabs-1'><i class="fa fa-globe"></i> Pengertian</a></li>
+                <li><a href='#tabs-2'><i class="fa fa-info-circle"></i> Tujuan</a></a></li>
+                <li><a href='#tabs-3'><i class="fa fa-briefcase"></i> Berita Online </a></a></li>
+              </ul>
+            </div>
+            <div class="col-lg-8">
+              <section class='tabs-content'>
+                <article id='tabs-1'>
+                  <img src="assets/images/about1.jpg" alt="">
+                  <h4>Pengertian</h4>
+
+                  <p>Laporan mengenai suatu peristiwa yang dikemas oleh media sebagi sarananya yang berisi mengenai sebuah peristiwa atau kejadian yang akan dipublikasikan berupa berita ( news )</p>
+                  <p>tahap penting bagi konvergensi media (media convergence). Berita online memiliki potensi untuk memanfaatkan berbagai fitur baru dari dunia pencampuran komunikasi media digital, di antaranya audio, video, animasi, dan peningkatan kontrol pengguna (user control). </p>
+
+                  <p>jenis berita yang disebarkkan secara online dalam bentuk teks, audio, video, dan live streaming.beritanya yang selalu diperbaharui secara berkelanjutan (continuous updates), memiliki interaktivitas, hypertext, dan multimedia.</p>
+                </article>
+                <article id='tabs-2'>
+                  <img src="assets/images/about2.jpg" alt="">
+                  <h4>Tujuan</h4>
+                  <p>digunakan sebagai sarana penyampaian informasi yang cepat, dan tidak terbatas ruang serta waktu. Misalnya masyarakat Indonesia bisa mengetahui peristiwa terkini yang sedang terjadi di Palestina. Begitu pula dengan warga negara lainnya yang bisa mengetahui informasi terkini tentang Indonesia.</p>
+                  <p>dengan berbagai fitur baru dari dunia pencampuran komunikasi media digital, di antaranya audio, video, animasi, dan peningkatan kontrol pengguna (user control). </p>
+                </article>
+                <article id='tabs-3'>
+                  <img src="assets/images/about3.jpg" alt="">
+                  <h4>Berita Online</h4>
+                  <p>Fusce laoreet malesuada rhoncus. Donec ultricies diam tortor, id auctor neque posuere sit amet. Aliquam pharetra, augue vel cursus porta, nisi tortor vulputate sapien, id scelerisque felis magna id felis. Proin neque metus, pellentesque pharetra semper vel, accumsan a neque.</p>
+
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro aut beatae commodi repudiandae distinctio, magnam blanditiis reiciendis vitae velit voluptatum natus, fugit quis eos dolores!</p>
+
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic adipisci reiciendis quaerat qui earum aut, atque esse quisquam quis exercitationem sapiente, dolorum consequatur consequuntur voluptatibus ipsam, fuga magnam beatae optio nam. Recusandae ut aliquid, eligendi.</p>
+                </article>
+              </section>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- ***** Our Classes End ***** -->
+
+      <section class="section" id="trainers">
+        <div class="container">
+          <h1><i class="fa fa-group" style="font-size:48px;color:black"></i>Team</i></h1>
           <br>
           <br>
           <div class="row">
-              <div class="col-md-3 col-sm-6">
-                  <div class="trainer-item">
-                      <div class="image-thumb">
-                          <img src="assets/images/1.jpg" alt="">
-                      </div>
-                      <div class="down-content">
-                          <span>Ketua</span>
-                          <h4>Intan Nur'aini</h4>
-                          <p>intanpermata2287@gmail.com
-                            Universitas Islam Sultan Agung
-                            Semarang</p>
-                          <ul class="social-icons">
-                              <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                              <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                              <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                              <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-                          </ul>
-                      </div>
-                  </div>
+            <div class="col-md-3 col-sm-6">
+              <div class="trainer-item">
+                <div class="image-thumb">
+                  <img src="assets/images/1.jpg" alt="">
+                </div>
+                <div class="down-content">
+                  <span>Ketua</span>
+                  <h4>Intan Nur'aini</h4>
+                  <p>intanpermata2287@gmail.com
+                    Universitas Islam Sultan Agung
+                    Semarang</p>
+                  <ul class="social-icons">
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                    <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                  </ul>
+                </div>
               </div>
-              <div class="col-md-3 col-sm-6">
-                  <div class="trainer-item">
-                      <div class="image-thumb">
-                          <img src="assets/images/team-image-2-646x680.jpg" alt="">
-                      </div>
-                      <div class="down-content">
-                          <span>Anggota</span>
-                          <h4>I Komang</h4>
-                          <p>Vestibulum id est eu felis vulputate hendrerit. Suspendisse dapibus turpis in dui pulvinar imperdiet. Nunc consectetur.</p>
-                          <ul class="social-icons">
-                              <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                              <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                              <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                              <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-                          </ul>
-                      </div>
-                  </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+              <div class="trainer-item">
+                <div class="image-thumb">
+                  <img src="assets/images/I Komang Krisnu Artha.webp" alt="">
+                </div>
+                <div class="down-content">
+                  <span>Anggota</span>
+                  <h4>I Komang Krisnu Artha</h4>
+                  <p>krisnuartha09@gmail.com
+                    Universitas Primakara
+                    <br>Bali
+                  </p>
+                  <ul class="social-icons">
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                    <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                  </ul>
+                </div>
               </div>
-              <div class="col-md-3 col-sm-6">
-                  <div class="trainer-item">
-                      <div class="image-thumb">
-                          <img src="assets/images/team-image-3-646x680.jpg" alt="">
-                      </div>
-                      <div class="down-content">
-                          <span>Anggota</span>
-                          <h4>Trisa Tiara</h4>
-                          <p>trisatiara123@gmail.com
-                            Politeknik Negeri Sriwijaya
-                            Palembang</p>
-                          <ul class="social-icons">
-                              <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                              <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                              <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                              <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-                          </ul>
-                      </div>
-                  </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+              <div class="trainer-item">
+                <div class="image-thumb">
+                  <img src="assets/images/team-image-3-646x680.jpg" alt="">
+                </div>
+                <div class="down-content">
+                  <span>Anggota</span>
+                  <h4>Trisa Tiara</h4>
+                  <p>trisatiara123@gmail.com
+                    Politeknik Negeri Sriwijaya
+                    Palembang</p>
+                  <ul class="social-icons">
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                    <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                  </ul>
+                </div>
               </div>
+            </div>
 
-              <div class="col-md-3 col-sm-6">
-                  <div class="trainer-item">
-                      <div class="image-thumb">
-                          <img src="assets/images/2.jpg" alt="">
-                      </div>
-                      <div class="down-content">
-                          <span>Anggota</span>
-                          <h4>Moch Rifki Adrianysah</h4>
-                          <p>mochrifki98620@gmail.com
-                            STMIK Mardira Indonesia
-                            Bandung</p>
-                          <ul class="social-icons">
-                              <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                              <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                              <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                              <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-                          </ul>
-                      </div>
-                  </div>
+            <div class="col-md-3 col-sm-6">
+              <div class="trainer-item">
+                <div class="image-thumb">
+                  <img src="assets/images/2.jpg" alt="">
+                </div>
+                <div class="down-content">
+                  <span>Anggota</span>
+                  <h4>Moch Rifki Adrianysah</h4>
+                  <p>mochrifki98620@gmail.com
+                    STMIK Mardira Indonesia
+                    Bandung</p>
+                  <ul class="social-icons">
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                    <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                  </ul>
+                </div>
               </div>
+            </div>
           </div>
-      </div>
-      </div> <!-- end main container -->
-  </section>
-    
-<
-   
+        </div>
+    </div> <!-- end main container -->
+    </section>
+    <!-- Footer -->
+    <?php include "footer.php" ?>
+    <!-- end footer -->
 
-    <div id="back-to-top">
+    < <div id="back-to-top">
       <a href="#top" aria-label="Go to top"><i class="ui-arrow-up"></i></a>
-    </div>
+      </div>
 
   </main> <!-- end main-wrapper -->
 
-  
+
   <!-- jQuery Scripts -->
   <script src="js/jquery.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
@@ -233,9 +237,10 @@
   <script src="js/owl-carousel.min.js"></script>
   <script src="js/flickity.pkgd.min.js"></script>
   <script src="js/twitterFetcher_min.js"></script>
-  <script src="js/jquery.newsTicker.min.js"></script>  
+  <script src="js/jquery.newsTicker.min.js"></script>
   <script src="js/modernizr.min.js"></script>
   <script src="js/scripts.js"></script>
 
 </body>
+
 </html>
