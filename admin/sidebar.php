697d4e86ca183ac1fa6579fc83f904543f6351f4
Trisa <trisatiara123@gmail.com>
2023-12-05 21:07:55 +0700

    tambah sidebar untuk admin

---
+++ admin/sidebar.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sidebar</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+    <!-- Sidebar -->
+	<section id="sidebar">
+		<a href="#" class="nav-link" id="brand">
+			<i class='bx bxs-smile'></i>
+			<span class="text">Admin</span>
+		</a>
+		<ul class="side-menu top">
+			<li>
+				<a href="index.php" class="nav-link">
+					<i class='bx bxs-home' ></i>
+					<span class="text">Dashboard</span>
+				</a>
+			</li>
+			<li>
+				<a href="berita.php" class="nav-link">
+					<i class='bx bxs-news'></i>
+					<span class="text">Berita</span>
+				</a>
+			</li>
+			<li>
+				<a href="kategori.php" class="nav-link">
+					<i class='bx bxs-category'></i>
+					<span class="text">Kategori</span>
+				</a>
+			</li>
+			<li>
+				<a href="penulis.php" class="nav-link">
+					<i class='bx bxs-user'></i>
+					<span class="text">Penulis</span>
+				</a>
+			</li>
+		</ul>
+		<ul class="side-menu">
+			<li>
+				<a href="proses_logout.php" class="nav-link" id="logout">
+					<i class='bx bxs-log-out-circle' ></i>
+					<span class="text">Logout</span>
+				</a>
+			</li>
+		</ul>
+	</section>
+</body>
+<script src="script.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</html>
