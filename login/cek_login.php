2f5435e32ff30b292fb36089273651fbd5be3438
Moch Rifki Adriansyah <mochrifki98620@gmail.com>
2023-12-10 10:24:18 +0700

    ubah login admin password default

--- login/cek_login.php
+++ login/cek_login.php
@@ -7,7 +7,7 @@ include '../koneksi.php';
  
 // menangkap data yang dikirim dari form
 $username = $_POST['username'];
-$password = $_POST['password'];
+$password = md5($_POST['password']);
  
 // menyeleksi data admin dengan username dan password yang sesuai
 $data = mysqli_query($conn,"select * from admin where username='$username' and password='$password'");
